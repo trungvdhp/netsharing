9b3fef50dcfaa92dc0b85717992c266499f1d0b8
trungvdt49 <trungvdt49@26ce40b9-3693-c574-95a7-f3637e1f9a0f>
2012-03-16 03:31:55 +0000

    add packet base and add class Constants

---
+++ trunk/client/source/src/base/Constants.java
@@ -0,0 +1,7 @@
+package base;
+
+public class Constants {
+	 public static final String KyTuChiaBanGhiDL = "@$%&";
+	 public static final String KyTuChiaTruongDL = "||||";
+	 public static final String Case = "CVM";
+}
